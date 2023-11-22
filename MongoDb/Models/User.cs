154d1bf47005fc002fffbc388611887752fa3feb
berdikulov-571 <mygithub571@gmail.com>
2023-11-22 17:17:20 +0500

    Добавьте файлы проекта.

---
+++ MongoDb/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDb.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ MongoDb/Program.cs
@@ -0,0 +1,37 @@
+﻿using MongoDb.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDb
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string connectionString = "mongodb://127.0.0.1:27017";
+            string databaseName = "demoDb";
+            string collectionName = "Users";
+
+            var client = new MongoClient(connectionString);
+            var db = client.GetDatabase(databaseName);
+            var collection = db.GetCollection<User>(collectionName);
+
+            //User user = new User()
+            //{
+            //    UserName = "Berdikulov",
+            //    FirstName = "Sanjarbek",
+            //    LastName = "Berdikulov",
+            //    Password = "sanjarbek2006",
+            //};
+
+            //await collection.InsertOneAsync(user);
+
+            var users = collection.Find(_ => true).ToList();
+
+            foreach (User i in users)
+            {
+                Console.Out.WriteLine($"{i.UserId}: {i.UserName} {i.Password}");
+            }
+        }
+    }
+}
