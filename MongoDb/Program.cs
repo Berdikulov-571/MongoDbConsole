b138c3ff67c648e392afcdeec325a1b2d186bc7a
berdikulov-571 <mygithub571@gmail.com>
2023-11-23 11:44:11 +0500

    Program.cs Updated

--- MongoDb/Program.cs
+++ MongoDb/Program.cs
@@ -1,6 +1,5 @@
 ﻿using MongoDb.Models;
-using MongoDB.Bson;
-using MongoDB.Driver;
+using MongoDb.Services;
 
 namespace MongoDb
 {
@@ -8,25 +7,7 @@ namespace MongoDb
     {
         static async Task Main(string[] args)
         {
-            string connectionString = "mongodb://127.0.0.1:27017";
-            string databaseName = "demoDb";
-            string collectionName = "Users";
-
-            var client = new MongoClient(connectionString);
-            var db = client.GetDatabase(databaseName);
-            var collection = db.GetCollection<User>(collectionName);
-
-            //User user = new User()
-            //{
-            //    UserName = "Berdikulov",
-            //    FirstName = "Sanjarbek",
-            //    LastName = "Berdikulov",
-            //    Password = "sanjarbek2006",
-            //};
-
-            //await collection.InsertOneAsync(user);
-
-            var users = collection.Find(_ => true).ToList();
+            IEnumerable<User> users = await GenericService.GetAllAsync<User>("Users");
 
             foreach (User i in users)
             {
