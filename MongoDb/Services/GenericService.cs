4f65e60c89e4dc683a28680bb07e78928abcb2d2
berdikulov-571 <mygithub571@gmail.com>
2023-11-23 11:44:03 +0500

    GenericService Created

---
+++ MongoDb/Services/GenericService.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Driver;
+
+namespace MongoDb.Services
+{
+    public class GenericService
+    {
+        static string connectionString = "mongodb://127.0.0.1:27017";
+        static string databaseName = "demoDb";
+
+        public static async ValueTask<IEnumerable<T>> GetAllAsync<T>(string collectionName)
+        {
+            MongoClient client = new MongoClient(connectionString);
+            var db = client.GetDatabase(databaseName);
+            var collection = db.GetCollection<T>(collectionName);
+
+            List<T> result = await collection.Find(_ => true).ToListAsync();
+
+            return result;
+        }
+
+        public static async ValueTask CreateAsync<T>(T model,string collectionName)
+        {
+            MongoClient client = new MongoClient(connectionString);
+            var db = client.GetDatabase(databaseName);
+            var collection = db.GetCollection<T>(collectionName);
+
+            await collection.InsertOneAsync(model);
+        }
+    }
+}
